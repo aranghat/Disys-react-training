65d824c0360abca911001e9c0677217f79bd1838
appsfarmer <appsfarmer@outlook.com>
2023-03-21 16:04:58 +0530

    Added Items

--- http-useeffect/src/App.js
+++ http-useeffect/src/App.js
@@ -15,19 +15,59 @@ export default function App(){
             })
              .catch((error) => {console.error(error);} );
     },[]);
-        
+
+    useEffect(() => setCurrentTitle(),[todos]);
+    
+    function setCurrentTitle(){
+        document.title = `(${todos.filter(d => d.completed == false).length}) - Pending`;
+    }
 
     function onItemAdd(newItem){
         setTodos([...todos,newItem])
     }
 
+    function onHandleComplete(id){
+        let item = todos.find((todo) => todo.id === id);
+        item.completed = true;
+        //Sent the put request to the server
+        axios.put(`http://localhost:8080/todos/${id}`,item)
+                .then((response) => {
+                    setTodos([...todos]);
+                })
+                .catch((error) => {console.error(error);});
+    }
+
+    function onHandleDelete(id){
+        axios.delete(`http://localhost:8080/todos/${id}`)
+                .then((response) => {
+                    let newArray = todos.filter(d => d.id != id);
+                    setTodos([...newArray]);
+                })
+                .catch((error) => {console.error(error);});
+    }
+
     return (
-        <div>
+        <div className="container">
             <NewToDoItem onAdd={onItemAdd} />
 
-            <ul>
+            <ul className="list-group">
                 {todos.map((todo) => 
-                    <li>{todo.name}</li>
+                    <li className="list-group-item" key={todo.id}>
+                        <div className="row">
+                            <div className="col">{todo.name}</div>
+                            <div className="col">{todo.completed ? "Completed" : "Pending"}</div>
+                            <div className="col-auto"><button 
+                            type="button" 
+                            className="btn btn-primary btn-sm"
+                            onClick={() => onHandleComplete(todo.id)}
+                            >Mark As Complete</button></div>
+                            <div className="col-auto">
+                                <button type="button"
+                                 onClick={() => onHandleDelete(todo.id)}
+                                 className="btn btn-danger btn-sm">Delete</button>
+                            </div>
+                        </div>
+                    </li>
                 )}
 
             </ul>
