fb71318ef059a8f727da7b03f113d0cb2ca3adc0
appsfarmer <appsfarmer@outlook.com>
2023-03-22 12:40:47 +0530

    Added ProductStore

---
+++ redux-basics/src/store/cartStore.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const cartStore = createSlice({
+    name: "cartStore",
+    initialState: {
+        cartItems: [],
+    },
+    reducers: {
+        addToCart: (state, action) => {
+            let product = action.payload;
+            this.state.cartItems.push(product);    
+        },
+        removeFromCart: (state, action) => {
+            let productId = action.payload;
+            this.state.cartItems = this.state.cartItems.filter(
+                (item) => item.id != productId
+            );
+        }
+    }
+});
