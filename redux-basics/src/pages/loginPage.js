24b53cc2427a4aaf2b37d19e3fa09c1449d61083
appsfarmer <appsfarmer@outlook.com>
2023-03-22 16:13:54 +0530

    Added Login Form

--- redux-basics/src/pages/loginPage.js
+++ redux-basics/src/pages/loginPage.js
@@ -1,10 +1,22 @@
 export default function LoginPage()
 {
     return(
-        <div>
-            <input type="text" />
-            <input type="password" />
-            <button type="submit">Login</button>
+        <div className="container">
+            <div className="form-group">
+                <label for="username">Email</label>
+                <input type="text" 
+                        id="username" 
+                        className="form-control"
+                        placeholder="Enter your email" />
+            </div>
+            <div className="form-group">
+                <label for="password">Password</label>
+                <input type="password" 
+                        className="form-control"
+                        id="username" 
+                        placeholder="Enter your password" />
+            </div>
+            <button type="submit" className="btn btn-primary mt-3">Login</button>
         </div>
     )
 }
