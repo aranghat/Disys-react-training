9b2f4f9ac63b4e64cccda4b93d2be05ce439b779
appsfarmer <appsfarmer@outlook.com>
2023-03-22 11:59:42 +0530

    Redux Dispatch

--- redux-basics/src/GuessResult.js
+++ redux-basics/src/GuessResult.js
@@ -1,7 +1,18 @@
+import { useSelector } from "react-redux"
+
 export default function GuessResult(){
+
+    let numberGuessed = useSelector(state =>
+                     state.numberstate
+                          .numberGuessed);
+
     return(
         <div>
-            <h1></h1>
+            {numberGuessed == 3 && 
+                <h1>You guessed it right</h1>}
+            {numberGuessed != 3 &&
+                <h1>You guessed {numberGuessed}
+                , it wrong</h1>}
         </div>
     )
 }
--- redux-basics/src/NumberGuessComponent.js
+++ redux-basics/src/NumberGuessComponent.js
@@ -1,9 +1,21 @@
+import { useDispatch } from "react-redux";
+import { setNumberGuessed } from "./MyState";
+import { useState } from "react";
+
 export default function NumberGuess(){
+
+    let [currentNumber,setCurrentNumber] = useState(0);
+    let dispatch = useDispatch();
+
+    function onGuess(){
+        dispatch(setNumberGuessed(currentNumber));
+    }
+
     return(
         <div>
              <h1>Guess the Number</h1>
-             <input type="number" />
-             <button type="button">Try Your Luck</button>
+             <input type="number" onChange={(event) => setCurrentNumber(event.target.value)} />
+             <button type="button" onClick={onGuess}>Try Your Luck</button>
         </div>
     );
 }
